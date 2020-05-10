ae2295e191f9797dab80465764836be6cc1271cb
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2020-05-10 16:58:36 -0300

    add api test

--- aiohttp_desafio/tests/test_desafio.py
+++ aiohttp_desafio/tests/test_desafio.py
@@ -1,4 +1,5 @@
 """Require running database server"""
+import json
 
 
 async def test_index(cli, tables_and_data):
@@ -57,3 +58,28 @@ async def test_state_results(cli, tables_and_data):
 async def test_no_exist_url(cli, tables_and_data):
     response = await cli.get('/no_exist_url')
     assert '404 Página não encontrada. O endereço que você solicitou não existe.' in await response.text()
+
+
+async def test_add_state(cli, tables_and_data):
+    response = await cli.post(
+        f'/api/state',
+        data=json.dumps({'state_name': 'Mato Grosso'})
+    )
+    assert response.status == 200
+    response = await cli.get('/')
+    assert response.status == 200
+    assert 'Mato Grosso' in await response.text()
+
+
+async def test_add_city(cli, tables_and_data):
+    response = await cli.post(
+        f'/api/city',
+        data=json.dumps({'city_name': 'Betim', 'state_id': '1'})
+    )
+    assert response.status == 200
+    response = await cli.get('state_city/1')
+    assert response.status == 200
+    assert 'Betim' in await response.text()
+    assert 'Belo Horizonte' in await response.text()
+    assert 'Sabará' in await response.text()
+    assert 'Minas Gerais' in await response.text()
